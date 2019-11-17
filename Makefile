09d43fcfa0c458aa60e9adbe1a5852fe6c098946
Raghul94 <sraghul1994@gmail.com>
2019-11-17 13:39:46 -0500

    Initial commit

--- Makefile
+++ Makefile
@@ -1,13 +1,13 @@
 CC = g++
-CFLAGS = -I.
+CFLAGS = -g,-pg,-I.
 
-DEPS = ray.h,vec3.h,hittable.h,sphere.h,camera.h
+DEPS = ray.h,vec3.h,hittable.h,sphere.h,camera.h,material.h
 
 %.o: %.c $(DEPS)
 		$(CC) -c -o $@ $< $(CFLAGS)
 
 main: main.o
-	$(CC) -o main main.o
+	$(CC) -o main main.o -pg
 
 clean: 
 	rm main main.o
--- main.cc
+++ main.cc
@@ -3,22 +3,25 @@
 #include "sphere.h"
 #include "hittable_list.h"
 #include "camera.h"
+#include "material.h"
 #include <float.h>
 
-vec3 random_point_in_unit_sphere(){
-    vec3 p;
-    do{
-        p = 2.0*vec3(drand48(),drand48(),drand48()) - vec3(1,1,1);
-    }while(p.squared_length() >= 1.0);
-    return p;
-}
-vec3 color(const ray &r, hittable *world)
+
+vec3 color(const ray &r, hittable *world, int depth)
 {
     hit_record rec;
     if (world->hit(r, 0.001, FLT_MAX, rec))
     {
+        ray scattered;
+        vec3 attenuation;
+        if(depth < 50 && rec.mat_ptr->scatter(r,rec,attenuation,scattered)){
+            return attenuation*color(scattered,world,depth+1);
+        }
+        else{
+            return vec3(0,0,0);
+        }
         vec3 target = rec.p + rec.normal + random_point_in_unit_sphere();
-        return 0.5*color(ray(rec.p,target - rec.p),world);
+        return 0.5*color(ray(rec.p,target - rec.p),world,depth+1);
     }
     else
     {
@@ -29,13 +32,15 @@ vec3 color(const ray &r, hittable *world)
 }
 int main()
 {
-    int nx = 2000, ny = 1000, ns = 100, ir, ig, ib;
+    int nx = 400, ny = 200, ns = 100, ir, ig, ib;
     float r, g, b;
     camera cam;
-    hittable *list[2];
-    list[0] = new sphere(vec3(0, 0, -1), 0.5);
-    list[1] = new sphere(vec3(0, -100.5, -1), 100);
-    hittable *world = new hittable_list(list, 2);
+    hittable *list[4];
+    list[0] = new sphere(vec3(-0.5, 0, -1), 0.5, new metal(vec3(1.0,0.3,0.3)));
+    list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8,0.8,0.0)));
+    list[2] = new sphere(vec3(0.5, 0, -1), 0.5, new metal(vec3(0.2,0.8,0.3)));
+    //list[3] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8,0.8,0.0)));
+    hittable *world = new hittable_list(list, 3);
     std::cout << "P3\n"
               << nx << " " << ny << "\n255\n";
     for (int j = ny - 1; j >= 0; j--)
@@ -48,7 +53,7 @@ int main()
                 float v = float(j + drand48()) / float(ny);
                 ray r = cam.get_ray(u, v);
                 vec3 p = r.point_at_parameter(2.0);
-                col += color(r, world);
+                col += color(r, world, 0);
             }
             col /= float(ns);
             col = vec3(sqrt(col[0]),sqrt(col[1]),sqrt(col[2]));
